fa538a8b5f5e2e8dc5c8ac7d4ae6c6b3e96f74ec
Michal Siber <michal.sib@seznam.cz>
2023-05-11 14:41:15 +0200

    Hotovo

---
+++ reseniCviceni.py
@@ -0,0 +1,11 @@
+print("Hello_World")
+##2
+jmenoprijmeni = "Michal Siber"
+
+##3
+cislopopisne = 45
+mesto = "Opava"
+ulice = "Na Pastvisku"
+
+##4
+print(f"Člověk {jmenoprijmeni} z ulice {ulice} s číslem popisným {cislopopisne} žije v městě {mesto}")
